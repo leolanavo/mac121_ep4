18d5619fe8e7d60246c199aabee48efdfbcd0a9e
Leonardo Lana <leo0198@gmail.com>
2016-11-02 17:57:42 -0200

    Create several directories

---
+++ include/readWord.h
@@ -0,0 +1,8 @@
+#ifndef READWORD_H
+#define READWORD_H
+
+/* Recieve a pointer a file, from where it will read a word,
+ * and return the read word in lower case. */
+readWord(FILE *file);
+
+#endif
