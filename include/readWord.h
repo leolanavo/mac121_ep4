2237495423b1527cb814ec299bf7097051472f30
leolanavo <leo0198@gmail.com>
2016-11-03 19:14:40 -0200

    Fix the missing semicolon

--- include/readWord.h
+++ include/readWord.h
@@ -1,10 +1,12 @@
 #ifndef READWORD_H
 #define READWORD_H
 
+#include <stdio.h>
+
 /* Recieve a pointer a file from where it will read a word,
  * and return the read word in lower case. It also recieves
  * an integer pointer so it can return 1 when the EOF is reached
  * end, will be 0, otherwise. */
-readWord(FILE *file);
+char* readWord(FILE* file, int* end);
 
 #endif
